9310b2fbc7c4ef07c3176ba3ce2989742977b598
jayden179 <Jayden.Starr983@schools.sa.edu.au>
2025-09-04 13:55:24 +0930

    Add files via upload
    Fixed java script code, no more issue

--- app.js
+++ app.js
@@ -1,7 +1,83 @@
-let userscore = 0;
-let computerscore = 0;
-const userscore_span = document.getElementById("user-score");
-const computerscore_span = document.getElementById("computer-score");
+
+// let userScore = 0;
+// let computerScore = 0;
+// const userScore_span = document.getElementById("user-score");
+// const computerScore_span = document.getElementById("computer-score");
+// const scoreboard_div = document.querySelector(".score-board");
+// const result_div = document.querySelector(".result");
+// const rock_div = document.getElementById("r");
+// const paper_div = document.getElementById("p");
+// const scissors_div = document.getElementById("s");
+
+// function getComputerChoice() {
+//      const choices = ['r', 'p', 's'];
+//      const randomNumber = Math.floor(Math.random() * 3);
+//      return choices[randomNumber];
+//  }
+
+//  function win() {
+//     console.log("Win!");
+//      userScore++;
+//      userScore_span.innerHTML = userScore;
+//      computerScore_span.innerHTML = computerScore;
+//  }
+
+//  function lose() {
+//      console.log("Lost");
+//  }
+
+//  function draw() {
+//      console.log("DRaw");
+//  }
+
+//  function game(userChoice) {
+//      const computerChoice = getComputerChoice();
+//      switch (userChoice + computerChoice) {
+//          case "rs":
+//          case "pr":
+//          case "sr":
+//              win();   
+//              break;
+//          case "rp":
+//          case "ps":
+//          case "sr":
+//               lose();
+//               break;
+//          case "rr":
+//          case "pp":
+//          case "ss":
+//              draw();
+//              break;
+
+//      }
+//  }
+
+
+
+//  function main() {
+
+//      rock_div.addEventListener('click', function() {
+//          game("r");
+//      })
+
+//      paper_div.addEventListener('click', function() {
+//          game("p");
+//      })
+
+//      scissors_div.addEventListener('click', function() {
+//          game("s");
+//      })
+//  }
+
+// main();
+
+// //video stamp 53:19
+
+
+let userScore = 0;
+let computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
 const scoreboard_div = document.querySelector(".score-board");
 const result_div = document.querySelector(".result");
 const rock_div = document.getElementById("r");
@@ -9,63 +85,69 @@ const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 
 function getComputerChoice() {
-    const choices = ['r', 'p', 's'];
-    const randomNumber = Math.floor(Math.random() * 3);
-    return choices[randomNumber];
-}
+     const choices = ['r', 'p', 's'];
+     const randomNumber = Math.floor(Math.random() * 3);
+     return choices[randomNumber];
+ }
 
-function win() {
-    userscore++;
-    userscore_span.innerHTML = userscore;
-    computerscore_span.innerHTML = computerscore;
-}
+ function win(user, computer) {
+    console.log("Win!");
+     userScore++;
+     userScore_span.innerHTML = userScore;
+     computerScore_span.innerHTML = computerScore;
+     console.log
+     console.log
+ }
 
-function lose() {
-    console.log("Lost");
-}
+ function lose() {
+     console.log("Lost");
+ }
 
-function Draw() {
-    console.log("DRaw");
-}
+ function draw() {
+     console.log("DRaw");
+ }
 
-function game(userChoice) {
-    const computerChoice = getComputerChoice();
-    switch (userChoice + computerChoice) {
-        case "rs":
-        case "pr":
-        case "sr":
-            win();   
-            break;
-        case "rp":
-        case "ps":
-        case "sr":
-             lose();
+ function game(userChoice) {
+     const computerChoice = getComputerChoice();
+     switch (userChoice + computerChoice) {
+         case "rs":
+         case "pr":
+         case "sr":
+             win(userChoice, computerChoice);   
              break;
-        case "rr":
-        case "pp":
-        case "ss":
-            Draw();
+         case "rp":
+         case "ps":
+         case "sr":
+              lose(userChoice, computerChoice);
+              break;
+         case "rr":
+         case "pp":
+         case "ss":
+             draw(userChoice, computerChoice);
+             break;
 
-            break;
+     }
+ }
 
-    }
-}
 
-function main() {
 
-    rock_div.addEventListener('click', function() {
-        game("r");
-    })
+ function main() {
 
-    paper_div.addEventListener('click', function() {
-        game("p");
-    })
+     rock_div.addEventListener('click', function() {
+         game("r");
+     })
 
-    scissors_div.addEventListener('click', function() {
-        game("s");
-    })
-}
+     paper_div.addEventListener('click', function() {
+         game("p");
+     })
+
+     scissors_div.addEventListener('click', function() {
+         game("s");
+     })
+ }
 
 main();
 
-//video stamp 53:19
+ //video stamp 53:19
+
+
