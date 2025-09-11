375afcd52f07011e97310deb2f17aeac80dc67b1
jayden179 <Jayden.Starr983@schools.sa.edu.au>
2025-09-11 13:41:22 +0930

    Add files via upload
    finished code

--- app.js
+++ app.js
@@ -1,93 +1,3 @@
-// let userScore = 0;
-// let computerScore = 0;
-// const userScore_span = document.getElementById("user-score");
-// const computerScore_span = document.getElementById("computer-score");
-// const scoreboard_div = document.querySelector(".score-board");
-// const result_p = document.querySelector(".result > p");
-// const rock_div = document.getElementById("r");
-// const paper_div = document.getElementById("p");
-// const scissors_div = document.getElementById("s");
-
-// function getComputerChoice() {
-//      const choices = ['r', 'p', 's'];
-//      const randomNumber = Math.floor(Math.random() * 3);
-//      return choices[randomNumber];
-//  }
-
-//  function convertToWord(Letter) {
-//     if (Letter === "r") return "rock";
-//     if (Letter === "p") return "paper";
-//     return "scissors";
-//  }
-
-//  function win(userChoice, computerChoice) {
-//     console.log("Win!");
-//      userScore++;
-//      userScore_span.innerHTML = userScore;
-//      computerScore_span.innerHTML = computerScore;
-//      const smallUserWord = '<sub><span style="font-size: smaller;">user</span></sub>';
-//      const smallCompWord = '<sub><span style="font-size: smaller;">comp</span></sub>';
-//      result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} beats ${convertToWord(computerChoice)}${smallCompWord} . you win 🔥`
-//  }
-
-//  function lose(userChoice, computerChoice) {
-//      console.log("Lost");
-//      computerScore++;
-//      userScore_span.innerHTML = userScore;
-//      computerScore_span.innerHTML = computerScore;
-//      const smallUserWord = '<sub><span style="font-size: smaller;">user</span></sub>';
-//      const smallCompWord = '<sub><span style="font-size: smaller;">comp</span></sub>';
-//      result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} loses to ${convertToWord(computerChoice)}${smallCompWord} . you lost 😭`
-//  }
-
-//  function draw(userChoice, computerChoice) {
-//      const smallUserWord = '<sub><span style="font-size: smaller;">user</span></sub>';
-//      const smallCompWord = '<sub><span style="font-size: smaller;">comp</span></sub>';
-//      result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} Equals ${convertToWord(computerChoice)}${smallCompWord} . Its a draw 😑`
-//  }
-
-//  function game(userChoice) {
-//      const computerChoice = getComputerChoice();
-//      switch (userChoice + computerChoice) {
-//          case "rs":
-//          case "pr":
-//          case "sr":
-//              win(userChoice, computerChoice);   
-//              break;
-//          case "rp":
-//          case "ps":
-//          case "sr":
-//               lose(userChoice, computerChoice);
-//               break;
-//          case "rr":
-//          case "pp":
-//          case "ss":
-//              draw(userChoice, computerChoice);
-//              break;
-
-//      }
-//  }
-
-
-
-//  function main() {
-
-//      rock_div.addEventListener('click', function() {
-//          game("r");
-//      })
-
-//      paper_div.addEventListener('click', function() {
-//          game("p");
-//      })
-
-//      scissors_div.addEventListener('click', function() {
-//          game("s");
-//      })
-//  }
-
-// main();
-
-
 let userScore = 0;
 let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
@@ -122,7 +32,6 @@ function getComputerChoice() {
      userChoice_div.classList.add('green-glow')
      setTimeout(() => userChoice_div.classList.remove('green-glow'), 1000);
  }
-
 
  function lose(userChoice, computerChoice) {
      const smallUserWord = '<sub><span style="font-size: smaller;">user</span></sub>';
@@ -168,22 +77,11 @@ function getComputerChoice() {
      }
  }
 
-
  function main() {
 
-     rock_div.addEventListener('click', function() {
-         game("r");
-     })
-
-     paper_div.addEventListener('click', function() {
-         game("p");
-     })
-
-     scissors_div.addEventListener('click', function() {
-         game("s");
-     })
+     rock_div.addEventListener('click', () => game("r"));
+     paper_div.addEventListener('click', () => game("p"))
+     scissors_div.addEventListener('click', () => game("s"))
  }
 
 main();
-
-// video time stamp at 1:26:20
